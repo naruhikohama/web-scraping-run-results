de9a4bc95bee9169b2a274f1521ce23ff863a98e
naruhikohama <naruhikohama@gmail.com>
2023-12-03 23:22:22 -0300

    adjusting script to scrape only new run results

--- tratamento-eventos.py
+++ tratamento-eventos.py
@@ -9,10 +9,12 @@ eventos_o2_bh = (
 )
 
 # print(eventos_o2_bh)
-
+resultados_o2_bh = pd.read_csv('resultados_ativo/data_scraped/corridas_o2_bh.csv')
 link_resultados = []
-for id in eventos_o2_bh['id_evento']:
-    link_resultados.append(resultados + str(id) + '&offset=0')
+for index, row in eventos_o2_bh.iterrows():
+    if row['nome'] not in resultados_o2_bh['nome_evento'].values:
+        id = row['id_evento']
+        link_resultados.append(resultados + str(id) + '&offset=0')
 
-with open("resultados_ativo/resultados_ativo/spiders/links_resultados.json", 'w') as f:
+with open("resultados_ativo/data_scraped/links_resultados.json", 'w') as f:
     json.dump(link_resultados, f)
