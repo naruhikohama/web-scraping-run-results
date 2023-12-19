747e9f16838cee89d8c220ff039929ab5e8ecffb
naruhikohama <naruhikohama@gmail.com>
2023-12-18 23:28:48 -0300

    extracting data with selenium

--- scraping_chronomax_selenium.py
+++ scraping_chronomax_selenium.py
@@ -18,6 +18,7 @@ df = (
 
 links = df['link'].tolist()
 
+corrida = df['prova'].tolist()[0].replace(" ", "_").lower()
 link_1 = links[0]
 
 print(link_1)
@@ -40,6 +41,46 @@ while count < 3:
 
 linhas_atletas = driver.find_elements('xpath', '//table[@id="tabres"]//tbody//tr')
 
+posicao = []
+num_peito = []
+nome = []
+equipe = []
+sexo = []
+categoria = []
+pos_categoria = []
+parcial_1 = []
+parcial_2 = []
+tempo_liquido = []
+tempo_bruto = []
+pace_medio = []
 for linha in linhas_atletas:
-    print(linha.text)
+    posicao.append(linha.find_element('xpath', './td[1]').text)
+    num_peito.append(linha.find_element('xpath', './td[2]').text)
+    nome.append(linha.find_element('xpath', './td[3]').text)
+    equipe.append(linha.find_element('xpath', './td[4]').text)
+    sexo.append(linha.find_element('xpath', './td[5]').text)
+    categoria.append(linha.find_element('xpath', './td[6]').text)
+    pos_categoria.append(linha.find_element('xpath', './td[7]').text)
+    parcial_1.append(linha.find_element('xpath', './td[8]').text)
+    parcial_2.append(linha.find_element('xpath', './td[9]').text)
+    tempo_liquido.append(linha.find_element('xpath', './td[10]').text)
+    tempo_bruto.append(linha.find_element('xpath', './td[11]').text)
+    pace_medio.append(linha.find_element('xpath', './td[12]').text)
+
+df = pd.DataFrame({
+    'posicao': posicao,
+    'num_peito': num_peito,
+    'nome': nome,
+    'equipe': equipe,
+    'sexo': sexo,
+    'categoria': categoria,
+    'pos_categoria': pos_categoria,
+    'parcial_1': parcial_1,
+    'parcial_2': parcial_2,
+    'tempo_liquido': tempo_liquido,
+    'tempo_bruto': tempo_bruto,
+    'pace_medio': pace_medio,
+})
+
+df.to_csv(f'chronomax_resultados/chronomax_{corrida}.csv', index=False)
 
